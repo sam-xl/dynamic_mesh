14ee0e1c87b0a64bf089d37a13ec44e7ff3d435a
Kate Twiss <k.v.twiss@tudelft.nl>
2025-03-18 11:40:21 +0100

    added prints to the entrypoint, and enabled automatic build and run

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 export CONTAINER_SCRIPTS_DIR=${DEV_WORKSPACE}/src/dynamic_mesh/container/docker_compose/scripts
 
 # ${CONTAINER_SCRIPTS_DIR}/bringup_container.sh
-# ${CONTAINER_SCRIPTS_DIR}/build_app.sh
-# ${CONTAINER_SCRIPTS_DIR}/run_app.sh
-${CONTAINER_SCRIPTS_DIR}/dev_app.sh
+echo "Building app..." && ${CONTAINER_SCRIPTS_DIR}/build_app.sh
+echo "Running app..." && ${CONTAINER_SCRIPTS_DIR}/run_app.sh
+# ${CONTAINER_SCRIPTS_DIR}/dev_app.sh
