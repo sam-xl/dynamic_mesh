db9ead9d591b9b088c5ac0ca2aff8f9c6f6be76b
Kate Twiss <k.v.twiss@tudelft.nl>
2025-03-18 11:42:07 +0100

    added prints to the entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 export CONTAINER_SCRIPTS_DIR=${DEV_WORKSPACE}/src/dynamic_mesh/container/docker_compose/scripts
 
-# ${CONTAINER_SCRIPTS_DIR}/bringup_container.sh
+#echo "Bringup container..." &&  ${CONTAINER_SCRIPTS_DIR}/bringup_container.sh
 echo "Building app..." && ${CONTAINER_SCRIPTS_DIR}/build_app.sh
 echo "Running app..." && ${CONTAINER_SCRIPTS_DIR}/run_app.sh
-# ${CONTAINER_SCRIPTS_DIR}/dev_app.sh
+# echo "starting dev mode..." && ${CONTAINER_SCRIPTS_DIR}/dev_app.sh
