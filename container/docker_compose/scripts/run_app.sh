63bb2e0f0434cd757678a17a79d8d2e702735542
Kate Twiss <k.v.twiss@tudelft.nl>
2025-03-18 11:55:31 +0100

    test mode config

--- container/docker_compose/scripts/run_app.sh
+++ container/docker_compose/scripts/run_app.sh
@@ -6,4 +6,8 @@ source ~/.bashrc
 source install/setup.bash
 
 # launch processes here
-ros2 run dynamic_mesh pcl_buffer_node --ros-args -r input_cloud:=/transformed_pointcloud -r output_mesh:=/cloud_mesh -p decay_time:=1
+# ros2 run dynamic_mesh pcl_buffer_node --ros-args -r input_cloud:=/transformed_pointcloud -r output_mesh:=/cloud_mesh -p decay_time:=1
+
+
+source install/setup.bash
+ros2 run dynamic_mesh pcl_buffer_node --ros-args -r input_cloud:=/scancontrol_pointcloud -r output_mesh:=/cloud_mesh -p decay_time:=1 -p do_transform:=true -p frame_id:=base_link
