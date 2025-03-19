eaff10d3063d7d1da06f205d3145828bea9156ab
Nikhil Sethi <sethi.nirvil@gmail.com>
2025-03-19 14:26:22 +0100

    Test and update dynamic_mesh
    - add right frame id
    - force domain id
    - fix build

--- container/docker_compose/scripts/build_app.sh
+++ container/docker_compose/scripts/build_app.sh
@@ -6,6 +6,8 @@ cd "${DEV_WORKSPACE}" || exit
 # export ROSDEP_SKIP_KEYS=snp_msgs 
 # "${DEV_WORKSPACE}"/src/dynamic_mesh/setup.sh -t "${DEV_WORKSPACE}"
 
+source /opt/ros/humble/setup.bash
+
 # install dependencies
 rosdep update
 rosdep install --from-paths src --ignore-src -r -y
--- container/docker_compose/scripts/run_app.sh
+++ container/docker_compose/scripts/run_app.sh
@@ -7,6 +7,6 @@ source install/setup.bash
 
 # launch processes here
 
-ros2 run dynamic_mesh pcl_buffer_node --ros-args -r input_cloud:=/scancontrol_pointcloud -r output_mesh:=/cloud_mesh -p decay_time:=1 -p do_transform:=true -p frame_id:=base_link
+ros2 run dynamic_mesh pcl_buffer_node --ros-args -r input_cloud:=/scancontrol_pointcloud -r output_mesh:=/cloud_mesh -p decay_time:=1 -p do_transform:=true -p frame_id:=ur10e_base_link
 
 # Make sure there's a valid TF between laser_frame and frame_id
